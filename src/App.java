82f26aa95a9deeeeaa05f0c4d34f47f05232fab7
Soupygolem <168599455+Soupygolem@users.noreply.github.com>
2024-08-30 21:43:08 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Tucker Schoppe, and i am 15 years old.");
+        System.out.println(" My Parents names are Jewel and Chistapher schoppe");
+        System.out.println(" I have four sibling");
+        System.out.println(" My older sibling is Taylor");
+        System.out.println("My twin is names Luke");
+        System.out.println("The yougest child is names Samuel");
+    }
+}
